bb89e2115692c16c65f2a5e1e6f578ff664a969f
george-tr6 <george.tr6@gmail.com>
2018-09-10 16:09:48 -0700

    Import export demo complete

---
+++ app/add_to_dom.js
@@ -0,0 +1,8 @@
+import $ from 'jquery';
+
+export default function(type, text){
+    const elem = $(`<${type}>`, {
+        text: text
+    });
+    $('#root').append(elem);
+}
---
+++ app/math.js
@@ -0,0 +1,11 @@
+export function add(x, y){
+    return x + y;
+}
+
+export const subtract = (x, y) =>{
+    return x - y;
+}
+
+export const multiply = (x, y) => x * y;
+
+export const divide = (x, y) => y ? x/y : "Can't divide by zero";
